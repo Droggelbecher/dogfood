43f7a5c60782ad9f698bb2230b53d2b2f3264737
Henning Hasemann <henning@hasemail.de>
2023-03-31 19:53:54 +0200

    cobyla doc

--- src/optimize_cobyla.rs
+++ src/optimize_cobyla.rs
@@ -1,33 +1,43 @@
 
-use std::cmp::min;
+use cobyla::{fmin_cobyla, CstrFn};
+use crate::diet_problem::DietProblem;
 
-use argmin::core::{CostFunction, Error, Executor};
-use cobyla::{fmin_cobyla, CobylaSolver, CstrFn};
-
-use crate::{
-    diet_problem::DietProblem,
-    nutrient::Nutrient
-};
-
+// Our variables are one flat vector of values,
+// we interleave lower and upper rectangle sides for each of the dimensions,
+// these compute the indices into the variable vector.
 pub fn lower(i: usize) -> usize { i * 2 }
 pub fn upper(i: usize) -> usize { i * 2 + 1 }
 
 pub fn optimize_cobyla(problem: &DietProblem) {
 
+    // Cobyla constraints are CstrFn (constraint functions) defined
+    // as Fn(&[f64]) -> f64, in our case, closures (which each have their own unique concrete type, hence the dyn).
+    // In order to make these closures live longer than the scope they were defined in (which is
+    // the loop body), we need to put them in a box.
     let mut boxed_constraints = Vec::<Box::<dyn CstrFn>>::new();
 
+    // Create a constraint for each lower nutrient bound
     for (nutrient, minimum) in &problem.minima.nutrients {
+
+        // Make a closure that computes by how much we violate this bound
+        // and put it into the box. Move in the ref to `problem`.
         let constraint = Box::new(move |x: &[f64]| {
+            // Sum up over all ingredients i
+            //    The amount of nutrient `nutrient` in that ingredient times the lower rectangle
+            //    side of ingredient i
             let s: f64 = problem.ingredients
                 .iter()
                 .enumerate()
                 .map(|(i, ingr)| { ingr.nutrients.get(&nutrient.clone()).unwrap_or(&0.0) * x[lower(i)] })
                 .sum();
+
+            // ... and demand it is >= the minimum
             f64::min(minimum.clone() - s, 0.0)
         });
         boxed_constraints.push(constraint);
     }
 
+    // Analog for upper bounds
     for (nutrient, maximum) in &problem.maxima.nutrients {
         let constraint = Box::new(move |x: &[f64]| {
             let s: f64 = problem.ingredients
@@ -40,12 +50,14 @@ pub fn optimize_cobyla(problem: &DietProblem) {
         boxed_constraints.push(constraint);
     }
 
+    // Cobyla expects a Vec<&dyn CstrFn> which we can now obtain by "unboxing" the
+    // `boxed_constraints`.
+    // &** is because "reference to box of CstrFn" -> "reference to CstrFn"
     let constraints: Vec<&dyn CstrFn> = boxed_constraints.iter().map(|b| &**b).collect();
 
     fn cost(x: &[f64], _data: &mut ()) -> f64 {
 
         // Compute volume
-
         let mut volume = 1.0;
         for i in 0..(x.len() / 2) {
             // x has, for each ingredient, a lower bound and an upper bound
@@ -58,7 +70,10 @@ pub fn optimize_cobyla(problem: &DietProblem) {
         volume
     }
 
+    // For each dimension/ingredient we have a left- and right box side
     let mut x = vec![0.0; problem.ingredients.len() * 2];
+
+    // Actually run cobyla with some parameters
     let (status, x_opt) = fmin_cobyla(cost, &mut x, &constraints, (), 0.5, 1e-4, 2000, 1);
     println!("COBYLA status {:?} x_opt {:?}", status, x_opt);
 
